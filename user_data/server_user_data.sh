b11b630e2e93a77c6dd80e5bd4556db12fd30336
Maciel <gusiqueira@uolinc.com>
2022-11-02 19:32:41 -0300

    incluindo doc

--- user_data/server_user_data.sh
+++ user_data/server_user_data.sh
@@ -3,3 +3,5 @@ sudo -s
 sudo apt-get update
 sudo apt-get install -y ubuntu-desktop
 sudo apt-get install -y gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+
+#https://blog.knoldus.com/access-gui-of-aws-ec2-instance-of-ubuntu/ (doc install UI)
